0b60c64f26c3df3e7d290999c614d06281859635
Jaeyoung1234 <82758353+Jaeyoung1234@users.noreply.github.com>
2021-12-26 02:18:18 +0900

    Add files via upload

---
+++ Hash2.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class PhoneBook {
+    public static boolean solution(String[] phone_book) {
+
+        Arrays.sort(phone_book);
+        
+
+        for(int i=0; i<phone_book.length-1; i++){
+            if(phone_book[i].length() > phone_book[i+1].length()){
+                continue;
+            }
+            if(phone_book[i].equals(phone_book[i+1].substring(0, phone_book[i].length()))){
+                return false;
+            }
+        }
+        return true;
+    }
+    }
